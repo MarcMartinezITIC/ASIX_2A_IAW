54b0d4a618a78eb470a5d40baf044f2ce24db6de
Bonnie <fnafmarc@gmail.com>
2025-10-17 19:56:19 +0200

    medio_hecho

---
+++ exercicis_practica_php_m_martinez/Ex1/ex1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <title>Exericici_1</title>
+</head>
+<body>
+    <form action="/exercicis_practica_php_m_martinez/Ex1/ex1.php" method="POST">
+        <label for="nom">Nom:</label>
+        <input type="text" name="nom"> <br>
+        <label for="cognom">Cognom:</label>
+        <input type="text" name="cognom"><br>
+        <label for="e-mail">E-mail:</label>
+        <input type="text" name="e-mail"><br>
+        <label for="">Missatge de contacte:</label>
+        <textarea id="comentari"></textarea>
+        <input type="submit" value="Enviar">
+
+
+    </form>
+</body>
+</html>
