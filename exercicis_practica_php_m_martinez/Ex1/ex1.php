64fa661d07ff8b4208b1d5d770486fffc94facf7
Bonnie <fnafmarc@gmail.com>
2025-10-24 19:53:20 +0200

    Ex1: fet (falta personalitzar)

--- exercicis_practica_php_m_martinez/Ex1/ex1.php
+++ exercicis_practica_php_m_martinez/Ex1/ex1.php
@@ -1,21 +1,18 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="ca">
 <head>
     <title>Exericici_1</title>
 </head>
 <body>
-    <form action="/exercicis_practica_php_m_martinez/Ex1/ex1.php" method="POST">
-        <label for="nom">Nom:</label>
-        <input type="text" name="nom"> <br>
-        <label for="cognom">Cognom:</label>
-        <input type="text" name="cognom"><br>
-        <label for="e-mail">E-mail:</label>
-        <input type="text" name="e-mail"><br>
-        <label for="">Missatge de contacte:</label>
-        <textarea id="comentari"></textarea>
-        <input type="submit" value="Enviar">
-
-
+    <form action="/exercicis_practica_php_m_martinez/Ex1/resposta.php" method="POST">
+    Nom: <input type="text" name="nom"> <br> <br>
+    Cognom: <input type="text" name="cognom"> <br> <br>
+    E-mail: <input type="email" name="email"> <br> <br>
+    Missatge: <br>
+    <textarea name="missatge"></textarea>
+    <br>
+    <input type="submit">
     </form>
+    
 </body>
 </html>
