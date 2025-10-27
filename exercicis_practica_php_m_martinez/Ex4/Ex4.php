27bfb75fca50c7d77c0c81510d739ab2d1f3010a
MarcMartinezITIC <2024_marc.martinez@iticbcn.cat>
2025-10-27 17:17:01 +0100

    começat el 4

---
+++ exercicis_practica_php_m_martinez/Ex4/Ex4.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ca">
+<head>
+    <title>Enquesta musica</title>
+</head>
+<body>
+    <h1>Quin estil de musica t'agrada mes?</h1>
+    <form action="/exercicis_practica_php_m_martinez/Ex4/recomenacio.php" method="post">
+    <input type="radio" name="musica" value="rock">
+    <input type="radio" name="musica" value="pop">
+    <input type="radio" name="musica" value="rap">
+    <input type="radio" name="musica" value="nu-metal">
+    <input type="radio" name="musica" value="heavy-metal">
+
+    </form>
+    
+</body>
+</html>
